baf6f562e598deb1439404c15cada495a877e35b
kevinangkajaya <kevinangkajaya@yahoo.com>
2024-07-13 02:31:38 +0700

    test remove site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://dog_vs_cat.github.io/',
-});
+export default defineConfig({});
