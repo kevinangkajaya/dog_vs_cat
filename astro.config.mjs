477e5f31852f784ddfa31b27b3dba9019cf5fe33
kevinangkajaya <kevinangkajaya@yahoo.com>
2024-07-13 23:09:42 +0700

    add base to dog_vs_cat for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // this will result in https://[site_name]/dog_vs_cat
+    base: 'dog_vs_cat'
+});
